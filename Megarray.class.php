57806b5e307ec4a33a063b42218077d4fc88a4d6
Alex Gisby <alex.gisby@gmail.com>
2010-04-30 22:19:09 +0100

    Megarray works a lot closer to inbuilt arrays.
    Setting null keys works. Numeric indexes are updated and tracked automatically. Debugging functions added in.

--- Megarray.class.php
+++ Megarray.class.php
@@ -7,13 +7,45 @@
 	
 	class Megarray implements Countable, Iterator, ArrayAccess
 	{
-		private $position 	= 0;
-		private $items 		= array();
+		private $position 			= 0;
+		private $next_numeric_idx	= 0;
+		private $items 		= array();		// The actual elements.
+		private $keys		= array();		// Keys are stored seperately.
+		private $original	= array();		// The original, untampered with version.
 		private $length 	= 0;
 		
 		public function __construct($arr = false)
 		{
+			if(is_array($arr))
+			{
+				foreach($arr as $key => $value)
+				{
+					$this->keys[] 	= $key;
+					$this->items[]	= $value;
+				}
+				$this->original = $arr;
+				$this->length = count($arr);
+			}
+		}
+		
+		public function __toString()
+		{
+			// Mimic print_r's functionality, loop through displaying all the elements;
+			return $this->debug(false);
+		}
+		
+		public function debug($echo = true)
+		{
+			// Prints a human readable version of the array:
+			$output	= 'Megarray Debug' . "\n";
+			$output .= '==============' . "\n";
+			$output .= 'Length: ' . $this->length . "\n";
+			$output .= '---- Elements ----' . "\n";
+			$output .= print_r($this->original, true);
 			
+			// Echo if the flag is set:
+			if($echo){		echo $output;		}
+			return $output;
 		}
 		
 		// Implement countable:
@@ -21,16 +53,92 @@
 		
 		// Implement Iterator:
 		public function rewind()	{		$this->position = 0;							}
-		public function current()	{		return $this->items[$this->position];			}		
-		public function key()		{		return $this->position;							}
+		public function current()	{		return $this->items[$this->position];			}	
+			
+		public function key()		
+		{		
+			return $this->keys[$this->position];
+		}
+		
 		public function next()		{		$this->position ++;								}
 		public function valid()		{		return isset($this->items[$this->position]);	}
 		
 		// Implement Array Access:
-		public function offsetSet($offset, $value)	{	$this->items[$offset] = $value;												}
+		public function offsetSet($offset, $value)	
+		{	
+			if($this->key_exists($offset))
+			{
+				// Key already exists so we're overwriting. Don't change length or anything.
+			}
+			else
+			{
+				if($offset === null)
+				{
+					// Auto-assign it the next position in the array:
+					$this->keys[] = $this->next_numeric_idx;
+					$this->original[$this->next_numeric_idx] = $value;
+					$this->next_numeric_idx ++;
+				}
+				else
+				{
+					if(is_numeric($offset))
+					{
+						// For numerics, we need to update the next_array_idx:
+						$this->next_numeric_idx = $offset + 1;
+					}
+					
+					$this->keys[] 	= $offset;
+					$this->original[$offset] = $value;
+				}
+				
+				$this->items[]	= $value;
+				$this->length ++;
+			}
+		}
+		
+		public function offsetGet($offset) 
+		{	
+			// Look up this key in the keys array:
+			$keypos = $this->get_key_index($offset);
+			if($keypos !== false && isset($this->items[$keypos]))
+			{
+				return $this->items[$keypos];
+			}
+			
+			return null;
+		}
+		
 		public function offsetExists($offset)		{	return isset($this->items[$offset]);										}
 		public function offsetUnset($offset)		{	unset($this->items[$offset]);												}
-		public function offsetGet($offset)			{	return isset($this->items[$offset])? $this->items[$offset] : null;			}
+		
+		private function get_key_index($search_key)
+		{
+			// array_search has problems when the values of an array are different types, so we have to do this
+			// manually.
+			foreach($this->keys as $idx => $key)
+			{
+				if($search_key === $key)
+				{
+					return $idx;
+				}
+			}
+			
+			return false;
+		}
+		
+		
+		public function key_exists($search_key)
+		{
+			foreach($this->keys as $idx => $key)
+			{
+				if($search_key === $key)
+				{
+					return (bool) $idx;
+				}
+			}
+			
+			return false;
+		}
 		
 		
 	}
